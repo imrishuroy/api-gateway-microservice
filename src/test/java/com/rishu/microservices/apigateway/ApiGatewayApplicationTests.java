61cd110462543c14c9637018fea3cde9691118cb
Rishu Kumar <rishu.kumar@maersk.com>
2023-07-13 18:40:10 +0530

    created jar and deployed to docker

---
+++ src/test/java/com/rishu/microservices/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rishu.microservices.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
